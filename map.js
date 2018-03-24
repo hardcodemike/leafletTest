11110273f075d9cd082cea1751d4e30387e03e55
Erharter Michael (MAC) <Michael.Erharter@fh-kufstein.ac.at>
2018-03-24 11:34:47 +0100

    changed map and added dependencies

--- map.js
+++ map.js
@@ -1,6 +1,22 @@
 var mymap = L.map('mapid').setView([47.583807, 12.1736679], 18); 
 //var map = L.map('map').setView([45.8167, 15.9833], 10);
 
+
+var LeafIcon = L.Icon.extend({
+    options: {
+       iconSize:     [38, 45],
+       iconAnchor:   [22, 94],
+       popupAnchor:  [-3, -76]
+    }
+});
+var openIcon = new LeafIcon({
+    iconUrl: './leaflet/images/knuckles.png',
+});
+
+var doneIcon = new LeafIcon({
+    iconUrl: './leaflet/images/heart.jpeg',
+});
+
 /*start lock map */
 mymap.dragging.disable();
 mymap.touchZoom.disable();
@@ -15,17 +31,27 @@ mymap.removeControl(mymap.zoomControl);
 /*end lock map */
 
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-}).addTo( mymap );
-var marker = L.marker([47.583807, 12.1736679]).bindPopup("test").addTo(mymap); //geolocation
-var markerTwo = L.marker([47.5839, 12.1736679]).bindPopup("test2").addTo(mymap);
+// L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+//   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+// }).addTo(mymap); //auswahl der verschiedenen Maps
+var ign = new L.tileLayer("http://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+    subdomains : ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
+    attribution: '&copy; <a href="http://basemap.at">Basemap.at</a>, <a href="http://www.isticktoit.net">isticktoit.net</a>'
+});
 
-map.locate({setView: true, minZoom: 18, maxZoom: 18});
+mymap.addLayer(ign);
+// TODO auf basemaps
+
+
+var marker = L.marker([47.583837, 12.171941], { icon: openIcon }).bindPopup("do u know the wae?").addTo(mymap); //geolocation
+var markerTwo = L.marker([47.583435, 12.172601], { icon: openIcon }).bindPopup("do u know the wae?").addTo(mymap); //geolocation
+var markerThree = L.marker([47.583674, 12.173288], { icon: openIcon }).bindPopup("do u know the wae?").addTo(mymap); //geolocation
+var markerFour = L.marker([47.584177, 12.172596], {icon: openIcon}).bindPopup("do u know the wae?").addTo(mymap); //geolocation
+var markerFive = L.marker([47.584260, 12.171722], {icon: openIcon}).bindPopup("do u know the wae?").addTo(mymap); //geolocation
 
 
 function onLocationFound(e) {
-    var radius = e.accuracy / 2;
+    var radius = e.accuracy / 4;
 
     L.marker(e.latlng).addTo(map)
         .bindPopup("You are within " + radius + " meters from this point").openPopup();
@@ -33,10 +59,12 @@ function onLocationFound(e) {
     L.circle(e.latlng, radius).addTo(map);
 }
 
-map.on('locationfound', onLocationFound);
+mymap.on('locationfound', onLocationFound);
+mymap.locate({setView: true, watch: true, minZoom: 18, maxZoom: 18});
+
 
 function onLocationError(e) {
     alert(e.message);
 }
 
-map.on('locationerror', onLocationError);
+mymap.on('locationerror', onLocationError);
